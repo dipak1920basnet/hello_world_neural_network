0a286cf66ad652b51c1ee53a46f37a23413bbda3
dipak1920basnet <dipak1920basnet@gmail.com>
2026-05-25 21:52:03 +0545

    simple nn in tf

---
+++ hello.py
@@ -0,0 +1,23 @@
+import tensorflow as tf 
+import numpy as np 
+
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.Input(shape=(1,)), #Decide the shape of input 
+        tf.keras.layers.Dense(units=1)
+    ]
+)
+
+model.compile(optimizer="sgd", loss="mean_squared_error")
+
+xs = np.array([    -7, -6, -5, -4, -3,
+    -2, -1, 0, 1, 2,
+    3, 4, 5, 6, 7], dtype=float)
+ys = np.array([-13, -11, -9, -7, -5,
+    -3, -1, 1, 3, 5,
+    7, 9, 11, 13, 15], dtype=float)
+
+model.fit(xs,ys, epochs=500)
+
+predictions = model.predict([10.0])
+print(predictions)
